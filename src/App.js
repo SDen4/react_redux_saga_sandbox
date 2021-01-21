216c7f01d4b9a75c3892c945ad9268f5a116c40a
SDen4 <asw325@yandex.ru>
2021-01-21 11:56:29 +0500

    add component Post Form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import PostForm from './components/PostForm';
+
 function App() {
-  return <div className="container">App</div>;
+  return (
+    <div className="container pt3">
+      <div className="row">
+        <div className="col">Form</div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <PostForm />
+        </div>
+        <div className="col">FETCH POSTS</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
