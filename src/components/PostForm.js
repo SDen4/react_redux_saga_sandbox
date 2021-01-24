c5e0f5bb91431061551cb72c433f065feff32797
SDen4 <asw325@yandex.ru>
2021-01-24 23:17:43 +0500

    add Alert

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { createPost } from '../redux/actions';
+import { createPost, showAlert } from '../redux/actions';
+
+import { Alert } from './Alert';
 
 class PostForm extends React.Component {
   constructor(props) {
@@ -17,12 +19,12 @@ class PostForm extends React.Component {
     const { title } = this.state;
 
     // protection of creation posts with empty titles
-    if (!title.trim()) return null;
+    if (!title.trim())
+      return this.props.showAlert(
+        'Post title can not be empty, please text the name.',
+      );
 
     const newPost = { title, id: Date.now().toString() };
-    console.log(newPost);
-
-    console.log(newPost.title);
 
     this.props.createPost(newPost);
 
@@ -42,29 +44,39 @@ class PostForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.submitHandler}>
-        <div className="form-group">
-          <label htmlFor="title">Post title</label>
-          <input
-            type="text"
-            className="form-control"
-            id="title"
-            placeholder="Enter the Post title"
-            value={this.state.title}
-            name="title"
-            onChange={this.changInputHandler}
-          />
-          <button className="btn btn-success" type="submit">
-            Add new Post
-          </button>
-        </div>
-      </form>
+      <>
+        {/* Alert */}
+        {this.props.alert && <Alert text={this.props.alert} />}
+        {/* Form */}
+        <form onSubmit={this.submitHandler}>
+          <div className="form-group">
+            <label htmlFor="title">Post title</label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              placeholder="Enter the Post title"
+              value={this.state.title}
+              name="title"
+              onChange={this.changInputHandler}
+            />
+            <button className="btn btn-success" type="submit">
+              Add new Post
+            </button>
+          </div>
+        </form>
+      </>
     );
   }
 }
 
 const mapDispatchToProps = {
   createPost,
+  showAlert,
 };
 
-export default connect(null, mapDispatchToProps)(PostForm);
+const mapStateToProps = (state) => ({
+  alert: state.loading.alert,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostForm);
--- src/redux/types.js
+++ src/redux/types.js
@@ -4,3 +4,6 @@ export const FETCH_POSTS = 'FETCH_POSTS';
 
 export const SHOW_LOADER = 'SHOW_LOADER';
 export const HIDE_LOADER = 'HIDE_LOADER';
+
+export const SHOW_ALERT = 'SHOW_ALERT';
+export const HIDE_ALERT = 'HIDE_ALERT';
