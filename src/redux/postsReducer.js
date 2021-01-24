7caf4aeff3cf3a9afd9b4519148ec40c8b22cf52
SDen4 <asw325@yandex.ru>
2021-01-24 15:54:01 +0500

    two variants of creation Post Reducer

--- src/redux/postsReducer.js
+++ src/redux/postsReducer.js
@@ -1,10 +1,18 @@
+import CREATE_POST from './types';
+
 const initialState = {
   posts: [],
   fetchedPosts: [],
 };
 
 const postsReduser = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case CREATE_POST:
+      // return {...state, posts: state.posts.concat(action.payload)}; // the first variant
+      return { ...state, posts: [...state.posts, action.payload] }; // the second variant
+    default:
+      return state;
+  }
 };
 
 export default postsReduser;
