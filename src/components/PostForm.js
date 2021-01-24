ec696d0c569566b2d93d1b7d81446c6cdcda5f6f
SDen4 <asw325@yandex.ru>
2021-01-24 13:31:50 +0500

    component PostForm

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -6,7 +6,26 @@ export default class PostForm extends React.Component {
     this.state = {};
   }
 
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   render() {
-    return <div>Post Form Component</div>;
+    return (
+      <form onSubmit={this.submitHandler}>
+        <div className="form-group">
+          <label htmlFor="title">Post title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            placeholder="Enter the Post title"
+          />
+          <button className="btn btn-success" type="submit">
+            Add new Post
+          </button>
+        </div>
+      </form>
+    );
   }
 }
