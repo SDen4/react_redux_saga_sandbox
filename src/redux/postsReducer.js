0a3199863654b85df4bb4f5b729edf71b924d7ee
SDen4 <asw325@yandex.ru>
2021-01-24 21:36:49 +0500

    FetchPosts reduser, action + jsonplaceholder for online data

--- src/redux/postsReducer.js
+++ src/redux/postsReducer.js
@@ -1,4 +1,5 @@
 import { CREATE_POST } from './types';
+import { FETCH_POSTS } from './types';
 
 const initialState = {
   posts: [],
@@ -10,6 +11,8 @@ const postsReduser = (state = initialState, action) => {
     case CREATE_POST:
       //   return { ...state, posts: state.posts.concat([action.payload]) }; // the first variant
       return { ...state, posts: [...state.posts, action.payload] }; // the second variant
+    case FETCH_POSTS:
+      return { ...state, fetchedPosts: action.payload }; // the second variant
     default:
       return state;
   }
--- src/redux/types.js
+++ src/redux/types.js
@@ -1,1 +1,6 @@
 export const CREATE_POST = 'CREATE_POST';
+
+export const FETCH_POSTS = 'FETCH_POSTS';
+
+export const SHOW_LOADER = 'SHOW_LOADER';
+export const HIDE_LOADER = 'HIDE_LOADER';
