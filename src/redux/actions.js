c5c94a835fd494c6549f72da5efc00c75c89a7a4
SDen4 <asw325@yandex.ru>
2021-05-05 20:31:39 +0500

    server errors to alert

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -46,12 +46,17 @@ export function hideAlert() {
 
 export function fetchPost() {
   return async (dispatch) => {
-    dispatch(showLoader());
-    const response = await fetch(
-      'https://jsonplaceholder.typicode.com/posts?_limit=5',
-    );
-    const json = await response.json();
-    dispatch(hideLoader());
-    dispatch({ type: FETCH_POSTS, payload: json });
+    try {
+      dispatch(showLoader());
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts?_limit=5',
+      );
+      const json = await response.json();
+      dispatch(hideLoader());
+      dispatch({ type: FETCH_POSTS, payload: json });
+    } catch (error) {
+      dispatch(showAlert('Something wrong...'));
+      dispatch(hideLoader());
+    }
   };
 }
